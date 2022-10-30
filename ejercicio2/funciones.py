87c69fff37a6493e030cd88b5aa7ddb3376b7d51
Germiprogramer <llorentemunozgerman@gmail.com>
2022-10-30 10:32:16 +0100

    commit

--- ejercicio2/funciones.py
+++ ejercicio2/funciones.py
@@ -1,7 +1,7 @@
 lista= [
     [1,2,3],
     [4,5,6],
-    [7,8,7]
+    [7,8,9]
 ]
 
 
@@ -13,6 +13,25 @@ def sarrus_iterativo(lista):
     for i in range(3):
         resultado -= lista[0][i%len(lista)]*lista[1][(i+1)%len(lista)]*lista[2][(i+2)%len(lista)]
     return resultado
+
+def determinante_recursivo(n, lista):
+    if n == 1:
+        return n
+    for i in range(n-1):
+        escalar= lista[0][i]
+        lista_nueva = []
+        for j in range(n-1):
+            lista_nueva.append(lista[j+1])
+            
+            lista_nueva[j].pop(i)
+        print(escalar * determinante_recursivo(n-1, lista_nueva))
+        return escalar * determinante_recursivo(n-1, lista_nueva)
+
+
+
+print(determinante_recursivo(3, lista))
+
+
 
 
 
